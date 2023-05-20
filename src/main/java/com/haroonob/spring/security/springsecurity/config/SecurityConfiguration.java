342a9a719de54c3dfc51d7b2787a27bc1aa2750d
haroonob <haroonob@gmail.com>
2023-05-20 12:30:52 +0300

    in memory user detail role base security implementation

---
+++ src/main/java/com/haroonob/spring/security/springsecurity/config/SecurityConfiguration.java
@@ -0,0 +1,119 @@
+package com.haroonob.spring.security.springsecurity.config;
+
+//import com.haroonob.spring.security.springsecurity.SecurityUserDetailsService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
+import java.util.Set;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable().
+//                securityMatcher("/register**")
+//                .authorizeHttpRequests(authConfig -> {
+//                    authConfig.anyRequest().authenticated();
+//                })
+//                .formLogin()
+//                .loginPage("/login").permitAll().and()
+//                .logout().invalidateHttpSession(true).clearAuthentication(true) .permitAll(); ;
+//
+//        return http.build();
+//    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+
+                .authorizeHttpRequests((authorize) ->
+
+                        authorize.anyRequest().authenticated()
+                ).formLogin(
+                        form -> form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/login")
+                                .successHandler(new AuthenticationSuccessHandler() {
+
+                                    @Override
+                                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                                        Authentication authentication) throws IOException, ServletException {
+                                        // run custom logics upon successful login
+
+                                        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+                                        String username = userDetails.getUsername();
+
+                                        String redirectURL = request.getContextPath();
+                                        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+                                        if (roles.contains("ROLE_ADMIN")) {
+                                            redirectURL = "/admin";
+                                        } else{
+                                            redirectURL = "/welcome";
+                                        }
+
+                                        response.sendRedirect(redirectURL);
+                                    }
+                                })
+                                .failureUrl("/error")
+                                .permitAll()
+                ).logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails ramesh = User.builder()
+                .username("haroon")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(ramesh, admin);
+    }
+}
+//
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider(SecurityUserDetailsService securityUserDetailsService) {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(securityUserDetailsService);
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//
+//        return authProvider;
+//    }
+
---
+++ src/main/java/com/haroonob/spring/security/springsecurity/controllers/WelComeController.java
@@ -0,0 +1,23 @@
+package com.haroonob.spring.security.springsecurity.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelComeController {
+
+    @GetMapping("/welcome")
+    public String greeting() {
+        return "welcome";
+    }
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+
+}
